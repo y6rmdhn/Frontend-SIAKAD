509e39ef4ff419dcd84e20d4180784fafc8be7c8
Muhamad Yopan Ramadhan <yopanramadhan8@gmail.com>
2025-02-20 00:03:38 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button"
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-pink-400 text-4xl">apk akuh</h1>
+      <Button>Test</Button>
+    </>
+  )
+}
+
+export default App
