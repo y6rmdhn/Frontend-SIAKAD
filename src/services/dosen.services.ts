e0de561f8e0f2effe924a804e2cad5f2a016104f
Muhamad Yopan Ramadhan <yopanramadhan8@gmail.com>
2025-05-30 02:09:16 +0700

    feat: fetching data riwayat

---
+++ src/services/dosen.services.ts
@@ -0,0 +1,54 @@
+import axiosInstance from "@/lib/axios/axiosInstance";
+import endpoint from "./endpoint.constant";
+
+const dosenServices = {
+    //   keluarga
+    getDataAnak: (page) =>
+        axiosInstance.get(`${endpoint.DOSEN}/anak`, {
+            params: {
+                page: page,
+            },
+        }),
+    getDataOrangtua: (page) =>
+        axiosInstance.get(`${endpoint.DOSEN}/orangtua`, {
+            params: {
+                page: page,
+            },
+        }),
+
+    // kepegawaian
+    getPangkat: (page) =>
+        axiosInstance.get(`${endpoint.DOSEN}/pangkat`, {
+            params: {
+                page: page,
+            },
+        }),
+    getJabatanAkademik: (page) =>
+        axiosInstance.get(`${endpoint.DOSEN}/jabatanakademik`, {
+            params: {
+                page: page,
+            },
+        }),
+    getJabatanFungsional: (page) =>
+        axiosInstance.get(`${endpoint.DOSEN}/jabatanfungsional`, {
+            params: {
+                page: page,
+            },
+        }),
+    getJabatanStruktural: (page) =>
+        axiosInstance.get(`${endpoint.DOSEN}/jabatanstruktural`, {
+            params: {
+                page: page,
+            },
+        }),
+    getHubunganKerja: (page) =>
+        axiosInstance.get(`${endpoint.DOSEN}/hubungankerja`, {
+            params: {
+                page: page,
+            },
+        }),
+
+//     Kompetensi
+};
+
+export default dosenServices;
