28da982124dcbe87023a63b5efac18ffe5b2aa46
wa1t <azkanyan@gmail.com>
2025-07-14 14:12:38 +0700

    fix provinsi/kota/kecamatan add pegawai

--- src/services/binderByte.services.ts
+++ src/services/binderByte.services.ts
@@ -2,19 +2,19 @@ import { wilayahApi } from "@/lib/axios/binderByteApiInstance.ts";
 
 const wilayahIdServices = {
     getProvinsi: async () => {
-        const response = await wilayahApi.get(`provinces.json`);
+        const response = await wilayahApi.get(`provinces`);
         return response.data;
     },
 
     getKota: async (provinceCode: string) => {
         if (!provinceCode) return []; // Kembalikan array kosong jika tidak ada kode
-        const response = await wilayahApi.get(`regencies/${provinceCode}.json`);
+        const response = await wilayahApi.get(`regencies/${provinceCode}`);
         return response.data;
     },
 
     getKecamatan: async (kotaCode: string) => {
         if (!kotaCode) return []; // Kembalikan array kosong jika tidak ada kode
-        const response = await wilayahApi.get(`districts/${kotaCode}.json`);
+        const response = await wilayahApi.get(`districts/${kotaCode}`);
         return response.data;
     },
 };
