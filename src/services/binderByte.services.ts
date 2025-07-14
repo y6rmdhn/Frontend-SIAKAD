9717a997a7a3a0af737b397ac76cd8de77ed2cf7
wa1t <azkanyan@gmail.com>
2025-07-14 15:11:23 +0700

    change code kab/kot, kec

--- src/services/binderByte.services.ts
+++ src/services/binderByte.services.ts
@@ -8,13 +8,21 @@ const wilayahIdServices = {
 
     getKota: async (provinceCode: string) => {
         if (!provinceCode) return []; // Kembalikan array kosong jika tidak ada kode
-        const response = await wilayahApi.get(`kabupaten/${provinceCode}`);
+        const response = await wilayahApi.get('kabupaten',{
+            params:{
+                id_provinsi: provinceCode
+            }
+        });
         return response.data.value || [];
     },
 
     getKecamatan: async (kotaCode: string) => {
         if (!kotaCode) return []; // Kembalikan array kosong jika tidak ada kode
-        const response = await wilayahApi.get(`kecamatan/${kotaCode}`);
+        const response = await wilayahApi.get('kecamatan'{
+            params:{
+                id_kabupaten: kotaCode
+            }
+        });
         return response.data.value || [];
     },
 };
