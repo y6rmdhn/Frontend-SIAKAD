960cdfb2b3056b2a398a3303ec648f8c71f9d030
wa1t <azkanyan@gmail.com>
2025-07-14 15:03:07 +0700

    fix provinsi

--- src/services/binderByte.services.ts
+++ src/services/binderByte.services.ts
@@ -3,19 +3,19 @@ import { wilayahApi } from "@/lib/axios/binderByteApiInstance.ts";
 const wilayahIdServices = {
     getProvinsi: async () => {
         const response = await wilayahApi.get(`provinsi`);
-        return response.data;
+        return response.data.value || [];
     },
 
     getKota: async (provinceCode: string) => {
         if (!provinceCode) return []; // Kembalikan array kosong jika tidak ada kode
         const response = await wilayahApi.get(`kabupaten/${provinceCode}`);
-        return response.data;
+        return response.data.value || [];
     },
 
     getKecamatan: async (kotaCode: string) => {
         if (!kotaCode) return []; // Kembalikan array kosong jika tidak ada kode
         const response = await wilayahApi.get(`kecamatan/${kotaCode}`);
-        return response.data;
+        return response.data.value || [];
     },
 };
 
